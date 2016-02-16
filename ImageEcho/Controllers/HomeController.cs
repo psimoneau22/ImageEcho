acc853e9e943a2dc08b6b99a9e6ba3c2bb9aa77f
Philip Simoneau <philip.simoneau@beeline.com>
2016-02-16 17:26:16 -0500

    up

---
+++ ImageEcho/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace ImageEcho.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            HttpPostedFileBase file = Request.Files[0];
+            string filePath = Path.Combine(Server.MapPath("~/Images/"), Path.GetFileName(file.FileName));
+            file.SaveAs(filePath);
+            return File(filePath, file.ContentType);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
