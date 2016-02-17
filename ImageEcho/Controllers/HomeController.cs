bf74931f9c61e7e2286a87ebe7a497c8386471a8
Philip Simoneau <philip.simoneau@beeline.com>
2016-02-17 18:11:03 -0500

    commit message

--- ImageEcho/Controllers/HomeController.cs
+++ ImageEcho/Controllers/HomeController.cs
@@ -4,18 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using System.Net;
 
 namespace ImageEcho.Controllers
 {
     public class HomeController : Controller
     {
         [HttpPost]
-        public ActionResult Upload()
+        public ActionResult Upload(string returnType, string ful, string med, string low)
         {
-            HttpPostedFileBase file = Request.Files[0];
-            string filePath = Path.Combine(Server.MapPath("~/Images/"), Path.GetFileName(file.FileName));
-            file.SaveAs(filePath);
-            return File(filePath, file.ContentType);
+            
+            if (Request.Files.Count > 0)
+            {
+                HttpPostedFileBase file = Request.Files[0];
+                string filePath = Path.Combine(Server.MapPath("~/Images/"), Path.GetFileName(file.FileName));
+                file.SaveAs(filePath);
+
+                if (returnType == "base64")
+                {
+                    return Content(Convert.ToBase64String(System.IO.File.ReadAllBytes(filePath)));
+                }
+
+                if (returnType == "url")
+                {
+                    return Content("~/Images/" + Path.GetFileName(file.FileName));
+                }
+
+                return File(filePath, file.ContentType);
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         }
 
         [HttpGet]
