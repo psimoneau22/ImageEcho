a9441dc684b1ae046dd829a25d2615d6a81675c9
Philip Simoneau <philip.simoneau@beeline.com>
2016-02-18 02:39:23 -0500

    updates

--- ImageEcho/Controllers/HomeController.cs
+++ ImageEcho/Controllers/HomeController.cs
@@ -11,26 +11,15 @@ namespace ImageEcho.Controllers
     public class HomeController : Controller
     {
         [HttpPost]
-        public ActionResult Upload(string returnType, string ful, string med, string low)
+        public ActionResult Upload()
         {
-            
-            if (Request.Files.Count > 0)
-            {
-                HttpPostedFileBase file = Request.Files[0];
-                string filePath = Path.Combine(Server.MapPath("~/Images/"), Path.GetFileName(file.FileName));
-                file.SaveAs(filePath);
-
-                if (returnType == "base64")
-                {
-                    return Content(Convert.ToBase64String(System.IO.File.ReadAllBytes(filePath)));
-                }
-
-                if (returnType == "url")
-                {
-                    return Content("~/Images/" + Path.GetFileName(file.FileName));
-                }
-
-                return File(filePath, file.ContentType);
+            foreach(string fileName in Request.Files)
+            {                
+                // we are sending up the original file and an optimized file, the optimized file is the first one                
+                HttpPostedFileBase file = Request.Files[fileName];
+                byte[] fileBytes = new byte[file.ContentLength];
+                file.InputStream.Read(fileBytes, 0, file.ContentLength);
+                return File(fileBytes, file.ContentType, file.FileName);
             }
 
             return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
