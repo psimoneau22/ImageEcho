ccac2cd202af39e79c0e46be9c649057cd4fd64e
Philip Simoneau <philip.simoneau@beeline.com>
2016-02-18 14:00:33 -0500

    removed unused code from fil upload

--- ImageEcho/Controllers/HomeController.cs
+++ ImageEcho/Controllers/HomeController.cs
@@ -13,24 +13,16 @@ namespace ImageEcho.Controllers
         [HttpPost]
         public ActionResult Upload()
         {
-            Dictionary<HttpPostedFileBase, byte[]> files = new Dictionary<HttpPostedFileBase, byte[]>();
-            foreach(string fileName in Request.Files)
-            {                
-                // we are sending up the original file and an optimized file, the optimized file is the first one                
-                HttpPostedFileBase file = Request.Files[fileName];
-                byte[] fileBytes = new byte[file.ContentLength];
-                file.InputStream.Read(fileBytes, 0, file.ContentLength);
-                file.SaveAs(Path.Combine(Server.MapPath("~/Images/"), DateTime.Now.ToString("hhmmss") + file.FileName));
-
-                files[file] = fileBytes;
+            if (Request.Files.Count > 0)
+            {
+                foreach (string fileName in Request.Files)
+                {
+                    // we are sending up the original file and an optimized file, the optimized file is the first one                
+                    HttpPostedFileBase file = Request.Files[fileName];
+                    file.SaveAs(Path.Combine(Server.MapPath("~/Images/"), DateTime.Now.ToString("hhmmss") + file.FileName));
+                }
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
             }
-
-            if (files.Count > 0)
-            {                
-                var first = files.First().Key;
-                return Content(string.Format("data:{0};base64,{1}", first.ContentType, Convert.ToBase64String(files[first])));
-            }
-            
 
             return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         }
